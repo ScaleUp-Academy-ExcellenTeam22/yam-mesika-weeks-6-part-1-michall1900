546a8b38822536ee1b77b8e9db31d9fda94c5e71
Michal Bucks <michall1900@gmail.com>
2022-05-10 19:02:16 +0300

    Fix documentation.

--- 6.2/my_filter.py
+++ 6.2/my_filter.py
@@ -3,20 +3,21 @@ from typing import Generator, Iterable, Iterator, Union
 
 
 def my_filter(function: Union[Callable, None], iterable: Iterable) -> Iterator[Generator]:
-    """
-    Implement filter function - receives function and iterable and return all of the
-    values that the function return for the value that weighed true.
+    """An implementation to filter function.
+
+    Receive a boolean function and an iterable.
+    Return all the iterable's values that the received function returns true while running on them.
     If None is received instead of a function, the function returns all values at the iterable that weighed true.
-    :param function: Any function that its return value could be weighed to true/ false. Could be None instead.
+    :param function: A function that its return value equivalent to true/ false.
     :param iterable: Any iterable.
-    :return: All of the values that its return value is weighed to true.
+    :return: All the values that its return value is equivalent to true.
     """
     return (item for item in iterable if (function and function(item)) or (not function and item))
 
 
 def main_my_filter() -> None:
-    """
-    Use my filter implementation to print some of its results.
+    """Use my filter implementation to print some of its results.
+
     :return: None.
     """
     print(list(my_filter(lambda x: x >= 18, [0, 1, 4, 10, 20, 35, 56, 84, 120])))
